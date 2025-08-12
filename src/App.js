a0073e39bbbd11d4b6089aeaac1c5a7c0ecae644
Shivram Reddy <shivramreddy999@gmail.com>
2025-08-12 21:18:58 +0530

    Revamp UI for Pustak Ghar

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Search from "./components/SearchBook";
 import Branch from "./components/SearchBranch";
-import SubjectsPage from "./components/SubjectSelect"
-import Content from "./components/Content"
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import SubjectsPage from "./components/SubjectSelect";
+import Content from "./components/Content";
 import SubjectDetails from "./components/SubjectDetails";
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
 function App() {
   return (
-    <Router >
-        <NavBar />
-        <div className="main">
-          <Routes basename="/">
-           <Route path="/" element={<Search />} />
-  <Route path="/branch-year" element={<Branch />} />
-  <Route path="/subjects" element={<SubjectsPage />} />
-  <Route path="/content" element={<Content />} />
-  <Route path="/subject-details" element={<SubjectDetails />} />
-
-          </Routes>
-        </div>
+    <Router>
+      <NavBar />
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/branch-year" element={<Branch />} />
+          <Route path="/subjects" element={<SubjectsPage />} />
+          <Route path="/content" element={<Content />} />
+          <Route path="/subject-details" element={<SubjectDetails />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
